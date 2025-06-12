5c0ed33c9f708026d76b03709efd807012435d46
Nada Bouknana <nada.bouknana@eurecom.fr>
2025-06-12 13:57:44 +0200

    tmp(needed to save) initial integration of cc inference in C++

---
+++ examples/xApp/c/positioning/ai_ml/cc_inference.cpp
@@ -0,0 +1,33 @@
+/*
+ * Licensed to the OpenAirInterface (OAI) Software Alliance under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The OpenAirInterface Software Alliance licenses this file to You under
+ * the OAI Public License, Version 1.1  (the "License"); you may not use this file
+ * except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.openairinterface.org/?page_id=698
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *-------------------------------------------------------------------------------
+ * For more information about the OpenAirInterface (OAI) Software Alliance:
+ *      contact@openairinterface.org
+ */
+
+// input is fixed to 8 by 100
+#include <torch/torch.h>
+
+// returns channel charting predictions
+int predict_cc_embedding()
+
+int main(int arcg, char* argv[])
+{
+   // runs inference and saves output in a csv file
+
+    return 0;
+}
