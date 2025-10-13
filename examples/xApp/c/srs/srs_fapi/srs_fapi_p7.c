efb81fa923299a919192c549f135b9e23f5b8db2
Nada Bouknana <nada.bouknana@eurecom.fr>
2025-10-13 17:12:46 +0200

    correct type of last_idx in FAPI unpacking function

--- examples/xApp/c/srs/srs_fapi/srs_fapi_p7.c
+++ examples/xApp/c/srs/srs_fapi/srs_fapi_p7.c
@@ -84,7 +84,7 @@ static uint8_t unpack_nr_srs_report_tlv_value(nfapi_srs_report_tlv_t *report_tlv
      }
    }
  #else
-  const uint16_t last_idx = ((report_tlv->length + 3) / 4) - 1;
+  const int16_t last_idx = ((report_tlv->length + 3) / 4) - 1;
   for (int i = 0; i < last_idx; i++) {
     if (!pull32(ppReadPackedMsg, &report_tlv->value[i], end)) {
       return 0;
